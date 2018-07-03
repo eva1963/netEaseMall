36e9515dc588348b6b19f357f5548ef7d55a5664
Laila06 <sias_xg_121@126.com>
2018-07-03 18:44:48 +0800

    Merge branch 'dev' of github.com:eva1963/netEaseMall into dev

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,14 +1,15 @@
 const {writeFile} = require('../utils/promiseFS'),
     STORE_PATH = './json/store.json';
 
-function ADD_STORE(req, res, goodsID) {
+function ADD_STORE(req, res, goodsID, count) {
     //=>把某一个商品存储到JSON中，实现加入购物车的功能
     let personID = req.session.personID,
         storeInfo = {
             id: req.storeDATA.length === 0 ? 1 : (parseFloat(req.storeDATA[req.storeDATA.length - 1].id) + 1),//=>ID自增长
             goodsID,
             personID,
-            state: 0,//=>默认是不支付
+            state: 0,//=>默认是未支付
+            count,//商品数量
             time: new Date().getTime()
         };
     //=>把数据先存放到原始数组中，最后把原始数组写入到JSON中永久保存
