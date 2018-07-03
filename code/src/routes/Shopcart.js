080b0eb8b0b2244c5d61e1b229faddf18cd1b05c
coyotena <shashahuo@163.com>
2018-07-03 21:53:59 +0800

    添加完成部分

--- code/src/routes/Shopcart.js
+++ code/src/routes/Shopcart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import '../static/less/shopCart.less'
-
+import NavBottom from '../component/NavBottom'
 
 class Shopcart extends React.Component {
     constructor(props,context){
@@ -10,7 +10,15 @@ class Shopcart extends React.Component {
     render(){
         return (
             <div className='shopCartBox'>
-                <div></div>
+                <div>
+                    <div className="shopTit">购物车</div>
+                    <ul className="shopTips">
+                        <li>30天无忧退货</li>
+                        <li>48小时快速退款</li>
+                        <li>满88元免邮费</li>
+                    </ul>
+                </div>
+                <NavBottom />
             </div>
         )
     }
