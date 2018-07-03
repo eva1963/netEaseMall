0bfc8eb017b9ff141d0ad00e1ea9342bd3633305
eva1963 <sias_xg_217@126.com>
2018-07-03 23:08:53 +0800

    冲突解决

--- code/src/store/action-types.js
+++ code/src/store/action-types.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-export const  CLASSIFY_QUERY_GOODS="CLASSIFY_QUERY_GOODS";
-=======
-export const PRODUCT_DETAIL_STEPINDEX = 'PRODUCT_DETAIL_STEPINDEX';   /*轮播图的index*/
-
-
 /*订单order*/
+export const CLASSIFY_QUERY_GOODS = "CLASSIFY_QUERY_GOODS";
+/*轮播图的index*/
+export const PRODUCT_DETAIL_STEPINDEX = 'PRODUCT_DETAIL_STEPINDEX';
 
 
->>>>>>> 4660e398d58b0b61ac9cded9e59d83f43705dd3a
