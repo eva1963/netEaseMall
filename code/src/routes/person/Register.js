bede898233d46b1a9bdb6d082c8816f2d881da4b
jt <fakesola@163.com>
2018-07-04 11:31:21 +0800

    页面完成登录注册接口

---
+++ code/src/routes/person/Register.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { Form, Button, Input, Icon } from 'antd';
+import md5 from 'blueimp-md5';
+
+import action from '../../store/action/index';
+
+import {register} from '../../api/person';
+
+const FormItem = Form.Item;
+
+class Register extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    handleSubmit=(ev)=>{
+        ev.preventDefault();
+        this.props.form.validateFieldsAndScroll(async (err, values) => {
+            if (!err) {
+                values.password = md5(values.password);
+                let result = await register(values);
+                if (parseFloat(result.code) === 0) {
+                    this.props.queryBaseInfo();
+                    this.props.history.push('/person/info');
+                    return;
+                }
+            }
+        });
+
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+
+        return <div className='personRegister'>
+
+            <div className='registerWrap'>
+                <span>帐号注册</span>
+            </div>
+
+            <div className='form'>
+                <Form className="register-form" onSubmit={this.handleSubmit}>
+                    <FormItem>
+                        {getFieldDecorator('userName', {
+                            rules: [
+                                {required: true, message: '请输入用户名!'}
+                            ]
+                        })(<Input prefix={<Icon type="user" />} placeholder="请输入用户名!" />)}
+                    </FormItem>
+
+                    <FormItem>
+                        {getFieldDecorator('userPass', {
+                            rules: [
+                                {required: true, message: '请输入密码!'}
+                            ]
+                        })(<Input prefix={<Icon type="lock" />} placeholder="请输入密码!" type="password" />)}
+                    </FormItem>
+
+                    <FormItem>
+                        <Button type="primary" htmlType="submit">注册</Button>
+                    </FormItem>
+
+                    <div className='agree'>
+                        <label>我同意 <a href="">《服务条款》</a>和<a href="">《网易隐私政策》</a></label>
+                    </div>
+                </Form>
+            </div>
+
+            <div className='registerfoot'>
+                <span>邮箱帐号注册</span>
+                <i></i>
+            </div>
+
+        </div>
+    }
+}
+// export default connect()(Register);
+export default Form.create()(connect(null, action.person)(Register));
--- code/src/store/action/person.js
+++ code/src/store/action/person.js
@@ -1,6 +1,13 @@
 /* 个人中心的action派发任务 */
 import * as TYPES from '../action-types';
+import {queryInfo} from '../../api/person';
 
-export default {
-
-}
+let person = {
+    queryBaseInfo() {
+        return {
+            type: TYPES.PERSON_QUERY_BASE,
+            payload: queryInfo()
+        }
+    }
+};
+export default person;
