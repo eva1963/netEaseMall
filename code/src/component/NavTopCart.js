38c693cea4ba78f77318574f20cf5ac1094e341d
eva1963 <sias_xg_217@126.com>
2018-07-04 18:49:20 +0800

    提交部分

--- code/src/component/NavTopCart.js
+++ code/src/component/NavTopCart.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import { Row, Col,Icon } from 'antd';
-import '../static/less/header.less'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Icon} from 'antd';
+import '../static/less/header.less';
 /*
-* @target:带购车车入口的header
-*/
-class NavTopCart extends React.Component{
-    constructor(props,context){
-        super(props,context);
-        this.state={
-            imgSrc:require('../static/img/neteasy.png')
+ * @target:带购车车入口的header
+ */
+class NavTopCart extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            imgSrc: require('../static/img/neteasy.png')
         }
     }
-    render(){
+
+    render() {
         return <div className={'navTopCart'}>
-            <div className={'home'}><Link to={"/home"}><Icon type="home"/></Link></div>
-                <img src={this.state.imgSrc}/>
-            <div className={'cart'}><Link to={"/order"}><Icon type="search"/></Link><Link to={"/order"}></Link>
-                <Link to={"/shopcart"}><Icon type="shopping-cart" /></Link></div>
+            <div className={'home'}>
+                <Link to={"/home"}><Icon type="home"/>
+                </Link>
+            </div>
+            <img src={this.state.imgSrc}/>
+            <div className={'cart'}>
+                <Icon type="search"/>
+                <Link to={"/shopcart"}><Icon type="shopping-cart"/></Link></div>
         </div>
     }
 }
 
-export default connect()(NavTopCart)
+export default NavTopCart;
