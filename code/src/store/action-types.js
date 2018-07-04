b7211c89d1b30c30ba8c381feb60761b233b30d6
coyotena <shashahuo@163.com>
2018-07-04 11:08:57 +0800

    解决action-types冲突

--- code/src/store/action-types.js
+++ code/src/store/action-types.js
@@ -1,13 +1,3 @@
-export const  CLASSIFY_QUERY_GOODS="CLASSIFY_QUERY_GOODS";
-
-export const PRODUCT_DETAIL_STEPINDEX = 'PRODUCT_DETAIL_STEPINDEX';   /*轮播图的index*/
-
-
-/*订单order*/
-
-
-/*订单order*/
-
 export const ORDER_UNPAY = 'ORDER_UNPAY';     /*未支付的订单*/
 export const ORDER_PAY = 'ORDER_PAY'; /*已支付的订单*/
 
@@ -16,7 +6,6 @@ export const CLASSIFY_QUERY_GOODS = "CLASSIFY_QUERY_GOODS";
 export const PRODUCT_DETAIL_STEPINDEX = 'PRODUCT_DETAIL_STEPINDEX';
 /*获取评价列表信息*/
 export const PRODUCT_DETAIL_COMMENTLIST = 'PRODUCT_DETAIL_COMMENTLIST';
-
 
 //购物车
 export const SHOP_CART_INFO = "SHOP_CART_INFO";
