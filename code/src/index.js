5d7fd35bac08f8a0e68f8a8838ae58707f0d9587
Laila06 <sias_xg_121@126.com>
2018-07-04 10:13:00 +0800

    解决冲突

--- code/src/index.js
+++ code/src/index.js
@@ -17,7 +17,7 @@ import store from './store';
 import Home from './routes/Home';
 import Classify from './routes/Classify';
 import Person from './routes/Person';
-import ShopCart from './routes/ShopCart';
+import ShopCart from './routes/Shopcart';
 import ProductDetail from './routes/ProductDetail';
 import CommentPage from './routes/proDetail/CommentPage';
 import Order from './routes/Order';
@@ -35,7 +35,7 @@ render(
                         <Route path="/productDetail" exact component={ProductDetail}/>
                         <Route path="/productDetail/commentList" component={CommentPage}/>
                         <Route path="/classify" component={Classify}/>
-                        <Route path="/shopcart" component={ShopCart}/>
+                        <Route path="/ShopCart" component={ShopCart}/>
                         <Route path="/person" component={Person}/>
                         <Route path="/order" exact component={Order}/> {/*订单页面*/}
                         <Redirect to="/home"/>
--- code/src/store/reducer/index.js
+++ code/src/store/reducer/index.js
@@ -14,7 +14,7 @@ let reducers = combineReducers({
     home,
     classify,
     person,
-    shopcart,
+    shopCart,
     prodetail,
     order,
 });
