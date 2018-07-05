f0e89c202ca8e31959c97046212bdb763faa1e12
eva1963 <sias_xg_217@126.com>
2018-07-05 12:05:48 +0800

    选规格

--- code/src/store/reducer/prodetail.js
+++ code/src/store/reducer/prodetail.js
@@ -2,11 +2,11 @@
 import * as TYPES from '../action-types';
 
 export default function (state = {
-   stepIndex: 1,
+    stepIndex: 1,
     commentList: [],
     goodsList: [],
     productInfo: {
-       color: '请选择商品规格',
+        color: '请选择商品规格',
         count: 1
     }
 }, action) {
@@ -14,20 +14,20 @@ export default function (state = {
     switch (action.type) {
         case TYPES.PRODUCT_DETAIL_STEPINDEX:
             state.stepIndex = action.stepIndex;
-             break;
+            break;
 
         case TYPES.PRODUCT_DETAIL_COMMENTLIST:
-            if(parseFloat(action.result.code) === 0){
+            if (parseFloat(action.result.code) === 0) {
                 state.commentList = action.result.data.result;
             }
             break;
         case TYPES.QUERY_GOODS_LIST:
-            if(parseFloat(action.result.code) === 0){
+            if (parseFloat(action.result.code) === 0) {
                 state.goodsList = action.result.data;
             }
             break;
         case TYPES.SET_PRODUCT_COMMERCIAL:
-            state.productInfo = Object.assign(state.productInfo,action.result);
+            state.productInfo = Object.assign(state.productInfo, action.result);
             break;
         default:
     }
