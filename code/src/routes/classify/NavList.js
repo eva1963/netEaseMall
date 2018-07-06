893888af093039d045f8f42f7987d8ec82707d77
BambooBai <17710445433@163.com>
2018-07-06 16:57:49 +0800

    navlist

--- code/src/routes/classify/NavList.js
+++ code/src/routes/classify/NavList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import Qs from 'qs';
-import action from '../../store/action/classify';
-import '../../static/less/orderTitle.less';
+import action from '../../store/action';
+import '../../static/less/navlist.less';
 
  class NavList extends  React.Component{
     constructor(props,context){
@@ -19,7 +19,8 @@ import '../../static/less/orderTitle.less';
         let {queryInfo,queryCategory}=this.props;
        console.log(this.props);
        let goodsData=[];
-       this.props.goodsData.length===0?goodsData=await queryInfo('all'):null;
+       this.props.goodsData.length===0?await queryInfo({type: 'all'}):null;
+       goodsData=this.props.goodsData;
        let categoryData=queryCategory(goodsData,this.state.curType);
        this.setState({
            goodsData,
@@ -31,8 +32,10 @@ import '../../static/less/orderTitle.less';
         if(this.props.goodsData===0)return '';
         let classifyData = ['推荐专区','爆品区','夏季专区','居家','鞋包配饰','服装','电器','洗护','银饰','餐厨','婴童','文体'],
             type = ['tuijianzhuanqu','baopinqu','xiajizhuanqu','jujia','xiebaopeishi','fuzhuang','dianqi','xihu','yinshi','canchu','yingtong','wenti'];
-        return <div>
-            <ul className={'navList_box orderTitle'}>
+        return <div className={'navList_box'}>
+            {/*导航*/}
+            <div className={'navlist_nav'}>
+            <ul className={'clearfix'}>
                 {
                     type.map((item,index)=>{
                         return <li key={index}>
@@ -41,6 +44,44 @@ import '../../static/less/orderTitle.less';
                     })
                 }
             </ul>
+            </div>
+            {/*商品信息*/}
+            <div className="navlist_bottom">
+
+                    {
+                        this.props.categorys.map(({type,category},index)=>{
+                            let newGoods=this.props.goodsData.filter(item=>item.type===type&&item.category===category);
+                           return <div className={'navlist_info'} key={index}>
+                                <div className={'navlist_title'}>
+                                    <p>{category}</p>
+                                    <p>描述</p>
+                                </div>
+
+                                   <ul className={'clearfix'}>
+                                   {
+                                       newGoods.map(({id,name,desc,pic,price,flag},index)=>{
+                                           return <li key={index}>
+                                               <img src={pic} alt=""/>
+                                               <div className={'navlist_desc'}>
+                                                   {desc}
+                                               </div>
+                                               <div className={'navlist_name'}>
+                                                   {name}
+                                               </div>
+                                               <div className={'navlist_price'}>
+                                                   {price}
+                                               </div>
+                                           </li>
+                                       })
+                                   }
+                                   </ul>
+                               </div>
+
+                        })
+                    }
+
+
+            </div>
         </div>;
     }
 }
